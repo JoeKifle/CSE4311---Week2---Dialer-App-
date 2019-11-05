11f35e1e429843c5184c4717376eeaa9ee0b7629
@joekifle <joey.kifle@gmail.com>
2019-11-05 20:00:04 +0300

    Final commit

--- app/src/main/java/com/example/john/dialarapp_week2/MainActivity.java
+++ app/src/main/java/com/example/john/dialarapp_week2/MainActivity.java
@@ -192,6 +192,7 @@ public class MainActivity extends AppCompatActivity {
             if(grantResults.length > 0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
 
                 makePhonecall();
+                edtPhoneNo.setText("");
 
 
             }else{
