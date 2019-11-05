90d673e65797e57f3c3ed94467074edaa15903ad
@joekifle <joey.kifle@gmail.com>
2019-11-05 19:47:26 +0300

    implement makePhonecall method

--- app/src/main/java/com/example/john/dialarapp_week2/MainActivity.java
+++ app/src/main/java/com/example/john/dialarapp_week2/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.john.dialarapp_week2;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import android.view.View;
@@ -8,8 +15,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CALL = 1;
 
     EditText edtPhoneNo;
     TextView lblinfo;
@@ -140,6 +150,60 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void btCallphone(View view){
+
+        makePhonecall();
+
+    }
+
+    public void makePhonecall(){
+
+        String number  = edtPhoneNo.getText().toString();
+
+        if(number.trim().length() > 0){
 
 
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CALL_PHONE},REQUEST_CALL);
+
+            }else{
+
+                String dial = "tel:"+number;
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+
+            }
+
+
+        }else{
+
+            Toast.makeText(this,"Please Enter Phone Number ",Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+
+        if(requestCode == REQUEST_CALL){
+
+            if(grantResults.length > 0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+
+                makePhonecall();
+
+
+            }else{
+
+                Toast.makeText(this,"Permission Denied",Toast.LENGTH_SHORT).show();
+
+
+            }
+
+
+        }
+
+
+    }
 }
